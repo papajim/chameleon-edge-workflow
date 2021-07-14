d3281a6b95b884e93ec7642e3420e9aeae5da9a0
Ryan Tanaka <ryanyt@hawaii.edu>
2021-07-14 12:25:08 -0700

    entrypoint fixes; set SEC_TOKEN_SYSTEM_DIRECTORY; added check for CONDOR_HOST env

--- condor9-arm64-isi-worker/entrypoint.sh
+++ condor9-arm64-isi-worker/entrypoint.sh
@@ -12,19 +12,20 @@ cd /work/htcondor/release_dir
     --force \
     --verbose
 
-#export CONDOR_CONFIG=/home/condor/50-test-setup.conf
-#/work/htcondor/release_dir/sbin/condor_store_cred -f \
-#     /home/condor/htcondor/etc/pool_password -p kevinMalone
-
-#cat /home/condor/50-test-setup.conf >> /home/condor/htcondor/etc/condor_config
 CONDOR_INSTALL_DIR=/home/condor/htcondor
+cat /home/condor/50-test-setup.conf >> $CONDOR_INSTALL_DIR/etc/condor_config
 if [[ $TOKEN ]]; then
     mkdir $CONDOR_INSTALL_DIR/tokens.d
+    echo "SEC_TOKEN_SYSTEM_DIRECTORY = $CONDOR_INSTALL_DIR/tokens.d" >> $CONDOR_INSTALL_DIR/etc/condor_config
     echo "$TOKEN" > $CONDOR_INSTALL_DIR/tokens.d/isi_token
 
     # token auth
     chmod 600 $CONDOR_INSTALL_DIR/tokens.d/isi_token
 fi 
+
+if [[ $CONDOR_HOST ]]; then
+    echo "CONDOR_HOST = $CONDOR_HOST" >> $CONDOR_INSTALL_DIR/etc/condor_config
+fi
 
 source /home/condor/htcondor/condor.sh
 cat /home/condor/htcondor/condor.sh >> ~/.bashrc
