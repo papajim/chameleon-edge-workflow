1a327d0a9bf8a4416ba8a0befb88ef5a2e83a92f
Ryan Tanaka <tanaka@isi.edu>
2021-07-16 12:51:22 -0700

    fix quoting in requirements; overriden condorpool site

--- test-workflow/workflow.py
+++ test-workflow/workflow.py
@@ -2,9 +2,22 @@
 import logging
 from pathlib import Path
 
+from Pegasus.api import *
+
 logging.basicConfig(level=logging.INFO)
 
 CURRENT_DIR = Path(__file__).resolve().parent
+
+props = Properties()
+props["pegasus.mode"] = "development"
+props["pegasus.transfer.worker.package"] = "false"
+props.write()
+
+sc = SiteCatalog()
+condorpool = Site("condorpool").add_env(PEGASUS_HOME="/home/condor/pegasus-5.1.0dev").add_pegasus_profile(style="condor")
+sc.add_sites(condorpool)
+sc.write()
+
 
 rc = ReplicaCatalog()
 rc.add_replica(site="local", lfn="input.txt", pfn=CURRENT_DIR / "input.txt")
@@ -24,8 +37,7 @@ wf = Workflow("test")
 cat_job = Job(cat)\
             .add_inputs(File("input.txt"))\
             .add_outputs(File("output.txt"))\
-            .add_env(PEGASUS_HOME="/home/condor/pegasus-5.1.0dev")\
-            .add_condor_profile(requirements="Arch == aarch64")
+            .add_condor_profile(requirements='Arch == "aarch64"')
 
 wf.add_jobs(cat_job)
 
